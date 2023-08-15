0420ae2caef72997a71c82c336fca26175de79b2
ObjectWorld <yuskim@gmail.com>
2023-08-16 00:34:01 +0900

    Commit all source code

---
+++ src/test/java/org/objectworld/docker/hello/HelloApplicationTests.java
@@ -0,0 +1,13 @@
+package org.objectworld.docker.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
